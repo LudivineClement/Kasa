c14dbd49efbd15c49b5c25398a2e56324be1dd47
ludivineclement <ludivine.clement0985@gmail.com>
2023-06-30 09:29:21 +0200

    finalisation_projet

--- src/components/Host.js
+++ src/components/Host.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Host = ({hostData}) => {
+const Host = ({ hostData }) => {
   return (
     <div>
       <div className='host'>
-      <p >{hostData.name}</p>
-      <img src={hostData.picture} alt={hostData.name} />
+        {/* Affiche le nom du propriétaire du bien grâce à la prop "hostData" */}
+        <p>{hostData.name}</p>
+        
+        {/* Affiche l'image du propriétaire */}
+        <img src={hostData.picture} alt={hostData.name} />
       </div>
-      
     </div>
   );
 };
--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Composant fonctionnel Tags qui prend une prop "content"
 const Tags = ({ content }) => {
   return <p className='tag'>{content}</p>;
 };
