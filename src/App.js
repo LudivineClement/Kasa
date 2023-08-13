43f0a7efb37af3075a255623bc03b16343e4e3c6
ludivineclement <ludivine.clement0985@gmail.com>
2023-08-13 11:17:26 +0200

    return_url_home

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/kasa' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path="/logement/:id" element={<Housing />} />
         <Route path='*' element={<Page404 />} />
