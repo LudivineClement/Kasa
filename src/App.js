2880e6f02bf77625c0ea4e79e3a84bfc7407a9a8
ludivineclement <ludivine.clement0985@gmail.com>
2023-08-13 11:21:08 +0200

    correction_browser_router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Page404 from './pages/Page404';
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
